b1be6ccd03ac66327e450aba0e8d789c2f971863
dxZhang <dxzhang49@gmail.com>
2023-12-08 13:38:50 +0800

    Replace ln with cat

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -3,5 +3,5 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "Creating tmux config..."
 mkdir -p ~/.config/tmux
-ln -s ./tmux.conf ~/.config/tmux/tmux.conf
+cat ./tmux.conf > ~/.config/tmux/tmux.conf
 
